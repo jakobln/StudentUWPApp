5c9cfb40b453e06da6fdc195a85cbd8d93a7b1e6
jakobln <62882745+jakobln@users.noreply.github.com>
2020-04-22 11:04:00 +0200

    Added student class

---
+++ StudentUWPApp/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentUWPApp
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _navn;
+
+        public string Navn 
+        {
+            get { return _navn; }
+            set { _navn = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
